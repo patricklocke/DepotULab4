a3fd58658111eb7955e5aad0e652dada77d308dc
Patrick Locke <patrick.s.locke@gmail.com>
2016-02-25 21:22:03 -0600

    removed broken and "noted out" code, added annotation

--- boxes.js
+++ boxes.js
@@ -1,9 +1,8 @@
 
 var btn = document.createElement('button');
 
-document.addEventListener('DOMContentLoaded', function() {
-    // var btn = document.createElement('button');
-    btn.style.color= 'white';
+document.addEventListener('DOMContentLoaded', function () {
+    btn.style.color = 'white';
     btn.style.backgroundColor = 'black';
     btn.style.marginBottom = "15px";
     btn.style.position = 'fixed';
@@ -11,15 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     btn.appendChild(text);
     document.body.appendChild(btn);
 });
-     
-// var markers = document.getElementsByClassName('mark');
-//     for (var i; i < markers.length; i++) {
-//         markers[i].addEventListener('click', boxCount);
-//     }
-//     function boxCount(event) {
-//         console.log(event.id);
-//     }
-
+//Create boxes on click
 btn.onclick = function boxes() {
     var markers = document.getElementsByClassName('mark');
     var newBox = document.createElement('div');
@@ -30,73 +21,43 @@ btn.onclick = function boxes() {
     newBox.style.cssFloat = "left";
     newBox.style.marginRight = "5px";
     newBox.style.marginTop = '35px';
-    newBox.className='mark';
+    newBox.className = 'mark';
     newBox.id = markers.length;
     document.body.appendChild(newBox);
-
-newBox.onmouseover = function reveal() { 
-var para = document.createElement('p');
-var text = document.createTextNode(newBox.id);
-para.style.color = 'white';
-para.style.textAlign = 'center';
-para.style.fontSize = '20px';
-para.style.paddingBottom = '25px';
-para.appendChild(text);
-newBox.appendChild(para);
-}
-newBox.onclick = function color() {
-    var colorArray = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'pink', 'gray', 'lime' ];
-    newBox.style.backgroundColor = colorArray[Math.floor((Math.random() * colorArray.length))];
-}
-
-newBox.ondblclick = function remove () {
-    var choice = parseInt(newBox.id);
-    if (choice % 2 === 1) {
-        var removeBox = (choice- 1)
-        var reBox = removeBox.toString();
-        var child = document.getElementById(reBox);
-        child.parentNode.removeChild(child);
-    } 
-    else {
-        var removeEven = (choice - 1)
-        var reEven = removeEven.toString();
-        var evenchild = document.getElementById(reEven);
-        evenchild.parentNode.removeChild(child);
+// change box on mouseover to reveal ID number
+    newBox.onmouseover = function reveal() {
+        var para = document.createElement('p');
+        var text = document.createTextNode(newBox.id);
+        para.style.color = 'white';
+        para.style.textAlign = 'center';
+        para.style.fontSize = '20px';
+        para.style.paddingBottom = '25px';
+        para.appendChild(text);
+        newBox.appendChild(para);
     }
-    
-    
-}
-//  Math.floor((Math.random() * 100) + 1);
+// change box color on single click
+    newBox.onclick = function color() {
+        var colorArray = ['red', 'blue', 'green', 'orange', 'yellow', 'purple', 'pink', 'gray', 'lime'];
+        newBox.style.backgroundColor = colorArray[Math.floor((Math.random() * colorArray.length))];
+    }
+// supposed to remove box to left if odd, right if even (could be backwards)
+// currently broken for even numbers
+    newBox.ondblclick = function remove() {
+        var choice = parseInt(newBox.id);
+        if (choice % 2 === 1) {
+            var removeBox = (choice - 1)
+            var reBox = removeBox.toString();
+            var child = document.getElementById(reBox);
+            child.parentNode.removeChild(child);
+        }
+        else {
+            var removeEven = (choice - 1)
+            var reEven = removeEven.toString();
+            var evenchild = document.getElementById(reEven);
+            evenchild.parentNode.removeChild(child);
+        }
+
+
+    }
+  
 }   
-
-
-// var para = document.createElement('p');
-// var text = document.createTextNode(newBox.id);
-// para.style.color = 'white';
-// para.appendChild(text);
-// newBox.appendChild(para);
-
-
-
-     // newBox.onmouseover = function display(i) {
-    //     t.color= 'white';
-    // }
-
- // var paras = docuemnt.getElementsByClassName('thing');
-    // for (var i , i < paras.length; i++) {
-    //     paras[i].addEventListener('click', someFunc);
-    // }
-   
-    //    someFunc(e) {
-    //        console.log(e.target.id);
-    //    }
-       
-       
-    //    var boxyArray = document.createElement('div');
-    //    boxyArray.className = 'thing';
-    //    document.body.appendChild(boxyArray);
-    
-    
-
-
-
